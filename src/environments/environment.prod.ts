964c847301baf83dbe1cfc27b23c7c818aa73af6
ILOZATUA <iliazatuashvili@gmail.com>
2021-07-22 11:27:39 +0400

    environment changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   movieApiBase: 'https://www.omdbapi.com/?apikey=540d1872',
   firebase: {
-    apiKey: 'AIzaSyCBVOiX1VE-AyQ2WLpQ8SiGQr_Al0uqOu4',
-    authDomain: 'movie-catalogue-a6dad.firebaseapp.com',
-    projectId: 'movie-catalogue-a6dad',
-    storageBucket: 'movie-catalogue-a6dad.appspot.com',
-    messagingSenderId: '367547911203',
-    appId: '1:367547911203:web:6ebce586199e03a8d37596',
+    apiKey: "AIzaSyCj4cxjyKAStu6d6jfbqnkkOmZffHxqwRI",
+    authDomain: "movie-catalogue-6ae1e.firebaseapp.com",
+    projectId: "movie-catalogue-6ae1e",
+    storageBucket: "movie-catalogue-6ae1e.appspot.com",
+    messagingSenderId: "299458533981",
+    appId: "1:299458533981:web:72d269923ae5201afc4bb4"
   },
 };
